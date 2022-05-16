9a850d4e41946eb895739fbda9f260b0ea587ded
Veronika Orlova <orlova.veron4@yandex.ru>
2022-05-16 21:23:08 +0300

    redux_lesson43

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { setTheme } from './redux/reducers/theme'
+import { useAppDispatch, useAppSelector } from './redux/hooks'
+
 
 function App() {
+  const theme = useAppSelector(state => state.theme.value)
+  const dispatch = useAppDispatch()
+
+  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.target)
+  }
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    dispatch(setTheme(newTheme))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App theme--${theme}`}>
+      <button text="Primary" className="primary" onClick={toggleTheme} />
     </div>
-  );
+  )
 }
 
 export default App;
